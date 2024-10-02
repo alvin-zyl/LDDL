58703c39289a42597043f16b532578993754f34e
Alvin Liu <ziyueliu@ucsb.edu>
2024-10-02 20:57:56 +0000

    Track the debug of line delimiter

--- shard_codebert_data.py
+++ shard_codebert_data.py
@@ -1,9 +1,8 @@
 import pickle, transformers, tqdm, os
 import numpy as np
 
-code, id, comment = pickle.load(open("/dataset/codebert/extracted_raw.pkl", "rb"))
-code_id_pair = list(zip(id, code))
-concated = list(map(lambda x: " ".join(x), code_id_pair))
+code, id, comment = pickle.load(open("/dataset/codebert/extracted_pair_raw.pkl", "rb"))
+concated = [" ".join(item) for item in zip(id, code)]
 num_codes = len(concated)
 np.random.seed(12345)
 shuffled_idx = np.random.choice(num_codes, num_codes, False)
@@ -16,5 +15,7 @@ for i in range(num_blocks):
     with open(os.path.join(output_dir, f"block_{i}.txt"), "w") as f:
         for idx in shuffled_idx[i * block_size : (i + 1) * block_size]:
             line = concated[idx]
+            if "\r\n" in line:
+                line = line.replace("\r\n", "\n")
             f.write(f"{line}{linedelimiter}")
     print(f"Block {i} finished")
